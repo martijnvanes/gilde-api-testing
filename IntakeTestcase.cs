58cbf856b8da89e314b47d23f1f2376284d3cf3a
martijnvanes <martijn.es@gmail.com>
2021-01-20 18:52:23 +0100

    Werkende testcase en niet werkende Post testcase

--- IntakeTestcase.cs
+++ IntakeTestcase.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using RestSharp;
+using RestSharp.Serialization.Json;
 using System;
+using System.Collections.Generic;
 
 namespace api_testing
 
@@ -14,6 +16,12 @@ namespace api_testing
             var request = new RestRequest("posts/13");
             var response = client.Get(request);
             Console.WriteLine(response.Content);
+
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            Console.WriteLine ("Deserialized output");
+            var result = output["au"]
+            Console.WriteLine(output);
         }
     }
 }
